56a9f2f7d28b2a10778de8c6143a1fe4567c0382
Dmitry Churkin <dmitrychurkin1988@gmail.com>
2024-03-31 22:27:25 +0100

    release-0.3.0: (#3)
    - add meta info on about;
    - increment patch version 0.3.0;

--- src/app/routes/about/about.component.ts
+++ src/app/routes/about/about.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { getName, getTauriVersion, getVersion } from '@tauri-apps/api/app';
+import { arch, locale, platform, type, version } from '@tauri-apps/api/os';
 
 @Component({
-  selector: 'app-about',
-  standalone: true,
-  imports: [],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+    selector: 'app-about',
+    standalone: true,
+    imports: [],
+    templateUrl: './about.component.html',
+    styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+    public metadata!: Readonly<{
+        name: string | null;
+        version: string | null;
+        tauriVersion: string | null;
+        os: string;
+    }>;
 
+    public async ngOnInit() {
+        const [
+            name,
+            appVersion,
+            tauriVersion,
+            osType,
+            osPlatform,
+            osArch,
+            osVersion,
+            osLocale
+        ] = await Promise.all([
+            getName,
+            getVersion,
+            getTauriVersion,
+            type,
+            platform,
+            arch,
+            version,
+            locale
+        ].map(fn => fn()));
+
+        this.metadata = {
+            name,
+            version: appVersion,
+            tauriVersion,
+            os: `${[
+                osType,
+                osPlatform,
+                osArch,
+                osVersion,
+                osLocale
+            ].join(' ')}`
+        };
+    }
 }
